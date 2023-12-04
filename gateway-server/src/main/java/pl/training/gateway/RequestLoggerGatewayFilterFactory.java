fb95723b6b8f6440445743e28d53be43b21d9bae
landrzejewski <landrzejewski.poczta@gmail.com>
2023-12-04 15:25:18 +0100

    x

---
+++ gateway-server/src/main/java/pl/training/gateway/RequestLoggerGatewayFilterFactory.java
@@ -0,0 +1,47 @@
+package pl.training.gateway;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Component
+public class RequestLoggerGatewayFilterFactory extends AbstractGatewayFilterFactory<RequestLoggerGatewayFilterFactory.Config> {
+
+    public RequestLoggerGatewayFilterFactory() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            Logger.getLogger(config.name)
+                    .log(config.level, "New request for path: %s".formatted(exchange.getRequest().getPath()));
+            return chain.filter(exchange);
+        };
+    }
+
+    @Override
+    public List<String> shortcutFieldOrder() {
+        return List.of("level", "name");
+    }
+
+    public static class Config {
+
+        private Level level;
+        private String name = RequestLoggerGatewayFilterFactory.class.getName();
+
+        public void setLevel(String level) {
+            this.level = Level.parse(level);
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+    }
+
+}
