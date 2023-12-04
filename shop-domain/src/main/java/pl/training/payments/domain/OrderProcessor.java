470120b4db2662c8dcf4cd5046950db31bfedb85
landrzejewski <landrzejewski.poczta@gmail.com>
2023-12-04 16:39:31 +0100

    x

---
+++ shop-domain/src/main/java/pl/training/payments/domain/OrderProcessor.java
@@ -0,0 +1,17 @@
+package pl.training.payments.domain;
+
+import lombok.extern.java.Log;
+import pl.training.payments.ports.ShopService;
+
+@Log
+public class OrderProcessor implements ShopService {
+
+    private static final String DEFAULT_CURRENCY = "PLN";
+
+    @Override
+    public void place(Order order) {
+        var totalValue = order.getTotalValue();
+        log.info("New order with total value: %d %s".formatted(totalValue, DEFAULT_CURRENCY));
+    }
+
+}
