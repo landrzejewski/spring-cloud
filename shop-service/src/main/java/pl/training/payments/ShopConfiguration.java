96b953cb35fbc4abba7fd77f899862f5d0af2d9d
landrzejewski <landrzejewski.poczta@gmail.com>
2023-12-05 12:07:35 +0100

    x

--- shop-service/src/main/java/pl/training/payments/ShopConfiguration.java
+++ shop-service/src/main/java/pl/training/payments/ShopConfiguration.java
@@ -8,11 +8,14 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import pl.training.payments.adapters.events.PaymentEventDto;
 import pl.training.payments.domain.ConstantDiscountCalculator;
 import pl.training.payments.domain.DiscountCalculator;
 import pl.training.payments.domain.OrderProcessor;
 import pl.training.payments.ports.PaymentsService;
 import pl.training.payments.ports.ShopService;
+
+import java.util.function.Consumer;
 
 @EnableFeignClients
 @Log
@@ -37,4 +40,8 @@ public class ShopConfiguration {
         return new ConstantDiscountCalculator(value);
     }
 
+    @Bean
+    public Consumer<PaymentEventDto> paymentEventsConsumer() {
+        return event -> log.info("New payment update: " + event.getPaymentId());
+    }
 }
