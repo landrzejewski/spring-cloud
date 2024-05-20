e828b30a53687db41352c2ac88c8756048677589
landrzejewski <landrzejewski.poczta@gmail.com>
2024-05-20 15:42:38 +0200

    config

--- notes.md
+++ notes.md
@@ -1,1 +1,6 @@
 - zaimplementuj filtr pozwalający na dostęp do systemu tylko w określonych godzinach
+
+- dodaj usługę shop-service pozwalającą na:
+    - dodanie zamówienia (lista zawierająca id, ilość i cenę produktów)
+    - podsumowanie całkowitej kwoty zamówienia
+- podłącz się do configuration-server i discovery-server
---
+++ shop-domain/src/main/java/pl/training/payments/domain/OrderProcessor.java
@@ -0,0 +1,19 @@
+package pl.training.payments.domain;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import pl.training.payments.ports.ShopService;
+
+@Log
+@RequiredArgsConstructor
+public class OrderProcessor implements ShopService {
+
+    private static final String DEFAULT_CURRENCY = "PLN";
+
+    @Override
+    public void place(Order order) {
+        var totalValue = order.getTotalValue();
+        log.info("New order with total value: %d %s".formatted(totalValue, DEFAULT_CURRENCY));
+    }
+
+}
