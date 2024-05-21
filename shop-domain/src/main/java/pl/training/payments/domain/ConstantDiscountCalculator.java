3111eb5de58021f66c37e11eda32d377cc7ac0a6
landrzejewski <landrzejewski.poczta@gmail.com>
2024-05-21 12:46:29 +0200

    config

---
+++ shop-domain/src/main/java/pl/training/payments/domain/ConstantDiscountCalculator.java
@@ -0,0 +1,14 @@
+package pl.training.payments.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class ConstantDiscountCalculator {
+
+    @Getter
+    private long value;
+
+}
--- shop-service/src/main/java/pl/training/payments/ShopConfiguration.java
+++ shop-service/src/main/java/pl/training/payments/ShopConfiguration.java
@@ -3,17 +3,16 @@ package pl.training.payments;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import pl.training.payments.adapters.ServiceResolver;
-import pl.training.payments.adapters.events.PaymentEventDto;
+import pl.training.payments.domain.ConstantDiscountCalculator;
 import pl.training.payments.domain.OrderProcessor;
 import pl.training.payments.ports.PaymentService;
 import pl.training.payments.ports.ShopService;
-
-import java.util.function.Consumer;
 
 @EnableFeignClients
 @Configuration
@@ -21,8 +20,8 @@ import java.util.function.Consumer;
 public class ShopConfiguration {
 
     @Bean
-    public ShopService shopService(PaymentService paymentService) {
-        return new OrderProcessor(paymentService);
+    public ShopService shopService(PaymentService paymentService, ConstantDiscountCalculator discountCalculator) {
+        return new OrderProcessor(paymentService, discountCalculator);
     }
 
     public ShopConfiguration(@Value("${discount}") int discount, ServiceResolver serviceResolver) {
@@ -40,5 +39,12 @@ public class ShopConfiguration {
     public Consumer<PaymentEventDto> paymentEventsConsumer() {
         return event -> log.info("Payment status updated (id: %s)".formatted(event.getPaymentId()));
     }*/
+
+    @RefreshScope
+    @Bean
+    public ConstantDiscountCalculator discountCalculator(@Value("${discount}") int discount) {
+        log.info("DiscountCalculator created...");
+        return new ConstantDiscountCalculator(discount);
+    }
 
 }
