82fb4606b77c720cfa897222db81fa862e17d810
landrzejewski <landrzejewski.poczta@gmail.com>
2024-05-21 10:23:06 +0200

    config

--- shop-service/src/main/java/pl/training/payments/adapters/payments/PaymentsApi.java
+++ shop-service/src/main/java/pl/training/payments/adapters/payments/PaymentsApi.java
@@ -3,13 +3,11 @@ package pl.training.payments.adapters.payments;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient("PAYMENTS-SERVICE")
-@RequestMapping("payments")
 public interface PaymentsApi {
 
-    @PostMapping
+    @PostMapping("payments")
     PaymentDto process(@RequestBody PaymentRequestDto paymentRequestDto);
 
 }
