8454a21a3dc6490d74dca1121342a6edd7e16cce
landrzejewski <landrzejewski.poczta@gmail.com>
2024-05-21 10:33:12 +0200

    config

--- notes.md
+++ notes.md
@@ -4,3 +4,6 @@
     - dodanie zamówienia (lista zawierająca id, ilość i cenę produktów)
     - podsumowanie całkowitej kwoty zamówienia
 - podłącz się do configuration-server i discovery-server
+
+- dodaj nową implementację adaptera usługi TimeProvider, tak aby czas/znacznik czasowy był pobierany z
+  zewnętrznego api http://worldtimeapi.org/api/timezone/Europe/Warsaw
--- shop-service/src/main/java/pl/training/payments/ShopConfiguration.java
+++ shop-service/src/main/java/pl/training/payments/ShopConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import pl.training.payments.adapters.ServiceResolver;
 import pl.training.payments.domain.OrderProcessor;
 import pl.training.payments.ports.PaymentService;
 import pl.training.payments.ports.ShopService;
@@ -21,8 +22,9 @@ public class ShopConfiguration {
         return new OrderProcessor(paymentService);
     }
 
-    public ShopConfiguration(@Value("${discount}") int discount) {
+    public ShopConfiguration(@Value("${discount}") int discount, ServiceResolver serviceResolver) {
         log.info("Current discount: " + discount);
+        log.info("Payments instance: " + serviceResolver.get("PAYMENTS-SERVICE"));
     }
 
     @LoadBalanced
