6790eb3e5111af3f81bf596a0421d8685d43ca35
landrzejewski <landrzejewski.poczta@gmail.com>
2024-05-21 09:59:07 +0200

    config

--- payments-service/src/main/java/pl/training/payments/PaymentsConfiguration.java
+++ payments-service/src/main/java/pl/training/payments/PaymentsConfiguration.java
@@ -1,7 +1,6 @@
 package pl.training.payments;
 
 import lombok.extern.java.Log;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pl.training.payments.adapters.time.SystemTimeProvider;
@@ -33,10 +32,6 @@ public class PaymentsConfiguration {
     @Bean
     public TimeProvider systemTimeProvider() {
         return new SystemTimeProvider();
-    }
-
-    public PaymentsConfiguration(@Value("${discount}") int discount) {
-        log.info("Current discount: " + discount);
     }
 
 }
--- shop-service/src/main/java/pl/training/payments/ShopConfiguration.java
+++ shop-service/src/main/java/pl/training/payments/ShopConfiguration.java
@@ -1,5 +1,6 @@
 package pl.training.payments;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pl.training.payments.domain.OrderProcessor;
@@ -11,6 +12,10 @@ public class ShopConfiguration {
     @Bean
     public ShopService shopService() {
         return new OrderProcessor();
+    }
+
+    public ShopConfiguration(@Value("${discount}") int discount) {
+        log.info("Current discount: " + discount);
     }
 
 }
