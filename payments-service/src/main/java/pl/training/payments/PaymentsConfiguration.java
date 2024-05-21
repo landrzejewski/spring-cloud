d4b332742bb9e5f9c50e7c34e3bd919258779250
landrzejewski <landrzejewski.poczta@gmail.com>
2024-05-21 10:56:49 +0200

    config

--- payments-service/src/main/java/pl/training/payments/PaymentsConfiguration.java
+++ payments-service/src/main/java/pl/training/payments/PaymentsConfiguration.java
@@ -2,7 +2,6 @@ package pl.training.payments;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import pl.training.payments.adapters.time.SystemTimeProvider;
 import pl.training.payments.domain.*;
 import pl.training.payments.ports.PaymentRepository;
 import pl.training.payments.ports.PaymentService;
@@ -25,11 +24,6 @@ public class PaymentsConfiguration {
     public PaymentService paymentService(PaymentIdGenerator paymentIdGenerator, PaymentFeeCalculator paymentFeeCalculator,
                                          PaymentRepository paymentRepository, TimeProvider timeProvider) {
         return new PaymentProcessor(paymentIdGenerator, paymentFeeCalculator, paymentRepository, timeProvider);
-    }
-
-    @Bean
-    public TimeProvider systemTimeProvider() {
-        return new SystemTimeProvider();
     }
 
 }
