b3f0a64cc1a2e03793dc6e0fbe65c035056517c0
landrzejewski <landrzejewski.poczta@gmail.com>
2024-05-22 14:43:29 +0200

    K

--- payments-service/src/main/java/pl/training/payments/SecurityConfiguration.java
+++ payments-service/src/main/java/pl/training/payments/SecurityConfiguration.java
@@ -4,13 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
-import pl.training.payments.security.KeycloakGrantedAuthoritiesMapper;
 import pl.training.payments.security.KeycloakJwtGrantedAuthoritiesConverter;
 import pl.training.payments.security.KeycloakLogoutHandler;
 
@@ -39,8 +37,6 @@ public class SecurityConfiguration {
 
                 .oauth2ResourceServer(config -> config.jwt(this::jwtConfig))
 
-                .oauth2Login(config -> config.userInfoEndpoint(this::userInfoCustomizer))
-
                 .logout(config -> config
                         .logoutUrl("/logout.html")
                         .logoutSuccessUrl("/index.html")
@@ -61,11 +57,5 @@ public class SecurityConfiguration {
         jwtConverter.setJwtGrantedAuthoritiesConverter(new KeycloakJwtGrantedAuthoritiesConverter());
         jwtConfigurer.jwtAuthenticationConverter(jwtConverter);
     }
-
-    // Client scopes -> Client scope details (roles) -> Mapper details (Realm access) -> Add to userinfo enabled (Keycloak Admin console)
-    private void userInfoCustomizer(OAuth2LoginConfigurer.UserInfoEndpointConfig userInfoEndpointConfig) {
-        userInfoEndpointConfig.userAuthoritiesMapper(new KeycloakGrantedAuthoritiesMapper());
-    }
-
 
 }
