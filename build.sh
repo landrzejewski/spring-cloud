83f2eb67f2f79cc39a3417d4004ddbbedd8dc877
landrzejewski <landrzejewski.poczta@gmail.com>
2024-05-23 11:46:30 +0200

    K

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 ./mvnw clean package -DskipTest
 docker build -t configuration-server ./configuration-server
 docker build -t discovery-server ./discovery-server
+docker build -t gateway-server ./gateway-server
