0b95a76ebd5ec103e3e40923f74d2e087476344a
landrzejewski <landrzejewski.poczta@gmail.com>
2024-05-23 12:17:27 +0200

    K

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ docker build -t configuration-server ./configuration-server
 docker build -t discovery-server ./discovery-server
 docker build -t gateway-server ./gateway-server
 docker build -t payments-service ./payments-service
+docker build -t shop-service ./shop-service
