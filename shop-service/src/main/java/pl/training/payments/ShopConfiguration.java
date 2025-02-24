ed834d412f31375b001c7365b75d5c46e8678666
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2025-02-24 15:57:24 +0100

    update

--- shop-service/src/main/java/pl/training/payments/ShopConfiguration.java
+++ shop-service/src/main/java/pl/training/payments/ShopConfiguration.java
@@ -1,6 +1,7 @@
 package pl.training.payments;
 
 import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pl.training.payments.domain.OrderProcessor;
@@ -9,6 +10,10 @@ import pl.training.payments.ports.ShopService;
 @Log
 @Configuration
 public class ShopConfiguration {
+
+    public ShopConfiguration(@Value("${discount}") long discount) {
+        log.info("Current discount: " + discount);
+    }
 
     @Bean
     public ShopService shopService() {
