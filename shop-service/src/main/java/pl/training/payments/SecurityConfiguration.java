3f3bd457b8a6505c644bf9c9d75db43257dc295f
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2025-02-25 16:20:12 +0100

    update

--- shop-service/src/main/java/pl/training/payments/SecurityConfiguration.java
+++ shop-service/src/main/java/pl/training/payments/SecurityConfiguration.java
@@ -4,11 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.client.OAuth2LoginConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
+import pl.training.payments.security.KeycloakGrantedAuthoritiesMapper;
 import pl.training.payments.security.KeycloakJwtGrantedAuthoritiesConverter;
+import pl.training.payments.security.KeycloakLogoutHandler;
 
 import java.util.List;
 
@@ -31,9 +35,15 @@ public class SecurityConfiguration {
                 .cors(config -> config.configurationSource(request -> corsConfiguration()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .oauth2ResourceServer(config -> config.jwt(this::jwtConfig))
-                .oauth2Login(config -> {})
+                .oauth2Login(config -> config.userInfoEndpoint(this::userInfoCustomizer))
                 .authorizeHttpRequests(config ->
-                        config.anyRequest().authenticated()
+                        config.anyRequest().hasRole("ADMIN")
+                )
+                .logout(config -> config
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/actuator")
+                        .invalidateHttpSession(true)
+                        .addLogoutHandler(new KeycloakLogoutHandler(new RestTemplate()))
                 )
                 .build();
     }
@@ -43,5 +53,16 @@ public class SecurityConfiguration {
         jwtConverter.setJwtGrantedAuthoritiesConverter(new KeycloakJwtGrantedAuthoritiesConverter());
         jwtConfigurer.jwtAuthenticationConverter(jwtConverter);
     }
+
+    // Client scopes -> Client scope details (roles) -> Mapper details (Realm access) -> Add to userinfo enabled (realm-roles)
+    private void userInfoCustomizer(OAuth2LoginConfigurer<HttpSecurity>.UserInfoEndpointConfig userInfoEndpointConfig) {
+        userInfoEndpointConfig.userAuthoritiesMapper(new KeycloakGrantedAuthoritiesMapper());
+    }
+
+    /*
+    Definiowanie First login flow dla logowania przez GitHub
+    Authentication -> Create flow (detect existing user flow, Basic flow) -> Add step (Detect existing broker user, Automatically set existing user)
+    Identity providers -> github -> First login flow -> detect existing user flow
+    */
 
 }
