c7e694a88a61d0896cc2fea148729d49a8049418
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2025-02-25 15:23:23 +0100

    update

---
+++ shop-service/src/main/java/pl/training/payments/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package pl.training.payments;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public CorsConfiguration corsConfiguration() {
+        var corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(List.of("http://localhost:4800"));
+        corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        corsConfig.setAllowedHeaders(List.of("*"));
+        corsConfig.setAllowCredentials(true);
+        return corsConfig;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(config -> config.configurationSource(request -> corsConfiguration()))
+                .csrf(AbstractHttpConfigurer::disable)
+                .oauth2ResourceServer(config -> config.jwt(jwtConfigurer -> {}))
+                .authorizeHttpRequests(config ->
+                        config.anyRequest().authenticated()
+                )
+                .build();
+    }
+
+}
