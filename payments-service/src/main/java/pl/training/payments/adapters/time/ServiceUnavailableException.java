f64e2889f5f666936b2091a17913037e6cab3213
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2025-02-25 19:16:30 +0100

    update

--- payments-service/src/main/java/pl/training/payments/adapters/time/ServiceUnavailableException.java
+++
@@ -1,4 +0,0 @@
-package pl.training.payments.adapters.time;
-
-public class ServiceUnavailableException extends RuntimeException {
-}
--- payments-service/src/main/java/pl/training/payments/adapters/time/SystemTimeProvider.java
+++ payments-service/src/main/java/pl/training/payments/adapters/time/SystemTimeProvider.java
@@ -1,12 +1,10 @@
 package pl.training.payments.adapters.time;
 
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import pl.training.payments.ports.TimeProvider;
 
 import java.time.Instant;
 
-@Primary
 @Service
 public class SystemTimeProvider implements TimeProvider {
 
