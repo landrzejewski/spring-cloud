7a2ccbc79ecfd84377e24e7a0add216d34ee4bb4
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2026-05-28 15:14:42 +0200

    update

---
+++ shop-service/src/main/java/pl/training/payments/security/SecurityConfiguration.java
@@ -0,0 +1,34 @@
+package pl.training.payments.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .oauth2ResourceServer(config -> config.jwt(this::jwtConfig))
+                .authorizeHttpRequests(config -> config
+                        .anyRequest().hasRole("ADMIN")
+                )
+                .build();
+    }
+
+    private void jwtConfig(OAuth2ResourceServerConfigurer<HttpSecurity>.JwtConfigurer jwtConfigurer) {
+        var jwtConverter = new JwtAuthenticationConverter();
+        jwtConverter.setJwtGrantedAuthoritiesConverter(new KeycloakJwtGrantedAuthoritiesConverter());
+        jwtConfigurer.jwtAuthenticationConverter(jwtConverter);
+    }
+
+}
