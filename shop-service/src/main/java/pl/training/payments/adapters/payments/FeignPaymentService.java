5716819483150c12276a70d73fe6aa9d709da856
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2026-05-28 09:34:03 +0200

    update

--- shop-service/src/main/java/pl/training/payments/adapters/payments/FeignPaymentService.java
+++ shop-service/src/main/java/pl/training/payments/adapters/payments/FeignPaymentService.java
@@ -1,9 +1,11 @@
 package pl.training.payments.adapters.payments;
 
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -13,28 +15,26 @@ import java.net.URI;
 import java.util.Currency;
 import java.util.Optional;
 
+@Primary
 @Log
 @Component
 @RequiredArgsConstructor
-public class RestTemplatePaymentService implements PaymentService {
+public class FeignPaymentService implements PaymentService {
 
-    private final RestTemplate restTemplate;
-
-    @Setter
-    @Value("${api.payments}")
-    private URI paymentsUri;
+    private final PaymentsApi paymentsApi;
 
     @Override
     public boolean pay(long amount, Currency currency) {
         var paymentRequestDto = new PaymentRequestDto("%d %s".formatted(amount, currency.getCurrencyCode()));
         try {
-            var paymentDto = restTemplate.postForObject(paymentsUri, paymentRequestDto, PaymentDto.class);
+            var paymentDto = paymentsApi.process(paymentRequestDto);
             return Optional.ofNullable(paymentDto)
                     .map(PaymentDto::isStarted)
                     .orElse(false);
-        } catch (RestClientException e) {
+        } catch (FeignException e) {
             log.info("Payment request could not be sent to payment service " + e.getMessage());
         }
         return false;
     }
+
 }
