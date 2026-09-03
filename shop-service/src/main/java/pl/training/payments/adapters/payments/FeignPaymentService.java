65be476b6c9264d36766c6561de6a1e270249152
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2026-09-03 09:56:34 +0200

    gateway server

---
+++ shop-service/src/main/java/pl/training/payments/adapters/payments/FeignPaymentService.java
@@ -0,0 +1,35 @@
+package pl.training.payments.adapters.payments;
+
+import feign.FeignException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import pl.training.payments.ports.PaymentService;
+
+import java.util.Currency;
+import java.util.Optional;
+
+@Primary
+@Log
+@Component
+@RequiredArgsConstructor
+public class FeignPaymentService implements PaymentService {
+
+    private final PaymentsApi paymentsApi;
+
+    @Override
+    public boolean pay(long amount, Currency currency) {
+        var paymentRequestDto = new PaymentRequestDto("%d %s".formatted(amount, currency.getCurrencyCode()));
+        try {
+            var paymentDto = paymentsApi.process(paymentRequestDto);
+            return Optional.ofNullable(paymentDto)
+                    .map(PaymentDto::isStarted)
+                    .orElse(false);
+        } catch (FeignException e) {
+            log.info("Payment request could not be sent to payment service " + e.getMessage());
+        }
+        return false;
+    }
+
+}
